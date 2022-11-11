30914b9e8a1e16cc578cbca800b0ed78574ed523
TruongGiaTien <truongtien577@gmail.com>
2022-11-11 19:10:28 +0700

    Finish css and showing the developer section

--- defi-react-app/src/App.js
+++ defi-react-app/src/App.js
@@ -4,16 +4,16 @@ import './App.css';
 import Navbar from './components/navbar/Navbar';
 import Hero from './components/hero/Hero';
 import About from './components/about/About';
-
+import Developers from './components/developer/Developer';
 
 function App() {
   return (
-    <div>
-     <Navbar/>
-     <Hero/>
-     <About/>
-
-    </div>
+      <div>
+        <Navbar/>
+        <Hero/>
+        <About/>
+        <Developers/>
+      </div>
   );
 }
 
