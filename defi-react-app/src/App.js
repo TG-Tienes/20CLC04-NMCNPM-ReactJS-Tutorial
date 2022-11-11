6f449079730e6146eb43fb404e018f24f9ae9f4e
TruongGiaTien <truongtien577@gmail.com>
2022-11-11 14:57:58 +0700

    Finish css for Header, Navigation

--- defi-react-app/src/App.js
+++ defi-react-app/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
 
+import Navbar from './components/navbar/Navbar';
+
 function App() {
   return (
     <div>
-     <h1> Heyy</h1>
+     <Navbar/>
     </div>
   );
 }
