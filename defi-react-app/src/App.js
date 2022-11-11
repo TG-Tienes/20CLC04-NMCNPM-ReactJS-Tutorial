bd5c256a5a5d07af5feb7a538803308605fe9c5f
TruongGiaTien <truongtien577@gmail.com>
2022-11-11 16:35:29 +0700

    Finish About section

--- defi-react-app/src/App.js
+++ defi-react-app/src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 
 import Navbar from './components/navbar/Navbar';
 import Hero from './components/hero/Hero';
+import About from './components/about/About';
 
 
 function App() {
@@ -10,6 +11,8 @@ function App() {
     <div>
      <Navbar/>
      <Hero/>
+     <About/>
+
     </div>
   );
 }
