2b46edf90d2d9523a2b56424e80b4609e479d0ed
TruongGiaTien <truongtien577@gmail.com>
2022-11-11 19:59:16 +0700

    Finish page footer

--- defi-react-app/src/App.js
+++ defi-react-app/src/App.js
@@ -6,6 +6,8 @@ import Hero from './components/hero/Hero';
 import About from './components/about/About';
 import Developers from './components/developer/Developer';
 import Subscribe from './components/subscribe/Subscribe';
+import Footer from './components/footer/Footer'
+
 
 function App() {
   return (
@@ -15,6 +17,7 @@ function App() {
         <About/>
         <Developers/>
         <Subscribe/>
+        <Footer/>
       </div>
   );
 }
